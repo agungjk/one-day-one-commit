8e13f3cb4782556254efa1057cf29d2e04320a3b
Agung Jati Kusumo <hiagungjk@gmail.com>
2021-07-06 15:48:24 +0700

    setup react native navigation

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import HomeScreen from './src/screens/home';
+
+const Stack = createStackNavigator();
+
+function CustomNavigationBar() {
+  return (
+    <Appbar.Header>
+      <Appbar.Content title="My awesome app" />
+    </Appbar.Header>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            header: CustomNavigationBar,
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>        
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
